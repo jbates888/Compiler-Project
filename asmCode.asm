554e0ab152dbe926ddd9eb66c6a7737f06ddb1a8
Jack <jackbates555@gmail.com>
2021-04-04 21:15:45 -0500

    fixed ||

--- asmCode.asm
+++ asmCode.asm
@@ -1,17 +1,15 @@
 	.text	
 	.globl		main
 main:
+	li		$t0, 0
+	sw		$t0, num0
 	li		$t0, 1
 	sw		$t0, num1
-	li		$t0, 2
-	sw		$t0, num2
-	li		$t0, 3
-	sw		$t0, num3
 	lw		$t0, num1
-	lw		$t1, num2
-	slt		$t2, $t0, $t1
-	sw		$t2, num4
-	lw		$t0, num4
+	lw		$t1, num1
+	and		$t2, $t0, $t1
+	sw		$t2, num3
+	lw		$t0, num3
 	li		$v0, 1
 	move		$a0, $t0
 	syscall	
@@ -19,10 +17,10 @@ main:
 	la		$a0, _nl
 	syscall	
 	lw		$t0, num1
-	lw		$t1, num3
-	sgt		$t2, $t0, $t1
+	lw		$t1, num1
+	and		$t2, $t0, $t1
 	beq		$zero, $t2, L1
-	li		$t0, 1000
+	li		$t0, 2
 	li		$v0, 1
 	move		$a0, $t0
 	syscall	
@@ -31,8 +29,8 @@ main:
 	syscall	
 L1:
 	lw		$t0, num1
-	lw		$t1, num1
-	sgt		$t3, $t0, $t1
+	lw		$t1, num0
+	and		$t3, $t0, $t1
 	beq		$zero, $t3, L2
 	li		$t0, 1000
 	li		$v0, 1
@@ -42,22 +40,10 @@ L1:
 	la		$a0, _nl
 	syscall	
 L2:
-	lw		$t0, num2
-	lw		$t1, num1
-	sgt		$t4, $t0, $t1
-	beq		$zero, $t4, L3
-	li		$t0, 2
-	li		$v0, 1
-	move		$a0, $t0
-	syscall	
-	li		$v0, 4
-	la		$a0, _nl
-	syscall	
-L3:
 	lw		$t0, num1
-	lw		$t1, num1
-	sge		$t5, $t0, $t1
-	beq		$zero, $t5, L4
+	lw		$t1, num0
+	or		$t4, $t0, $t1
+	beq		$zero, $t4, L3
 	li		$t0, 3
 	li		$v0, 1
 	move		$a0, $t0
@@ -65,11 +51,11 @@ L3:
 	li		$v0, 4
 	la		$a0, _nl
 	syscall	
-L4:
-	lw		$t0, num1
-	lw		$t1, num2
-	sge		$t6, $t0, $t1
-	beq		$zero, $t6, L5
+L3:
+	lw		$t0, num0
+	lw		$t1, num0
+	or		$t5, $t0, $t1
+	beq		$zero, $t5, L4
 	li		$t0, 1000
 	li		$v0, 1
 	move		$a0, $t0
@@ -77,37 +63,13 @@ L4:
 	li		$v0, 4
 	la		$a0, _nl
 	syscall	
-L5:
-	lw		$t0, num3
-	lw		$t1, num2
-	sge		$t7, $t0, $t1
-	beq		$zero, $t7, L6
-	li		$t0, 4
-	li		$v0, 1
-	move		$a0, $t0
-	syscall	
-	li		$v0, 4
-	la		$a0, _nl
-	syscall	
-L6:
-	li		$t0, 6
-	lw		$t1, num1
-	sgt		$t8, $t0, $t1
-	beq		$zero, $t8, L7
-	li		$t0, 5
-	li		$v0, 1
-	move		$a0, $t0
-	syscall	
-	li		$v0, 4
-	la		$a0, _nl
-	syscall	
-L7:
+L4:
 	li		$v0, 10
 	syscall	
 	.data	
 	.align		4
 _nl:	.asciiz		"\n"
+num0:	.word		0
 num1:	.word		0
-num2:	.word		0
 num3:	.word		0
 num4:	.word		0
